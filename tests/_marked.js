afd5a9557c522fa01e5cc195d91c2ee7e0498cd9
Mikael Brevik <mikaelbre@gmail.com>
2023-05-11 08:46:12 +0200

    feat: adds support for marked 5 (#201)
    * feat: adds support for marked 5
    
    * fix: cases where marked.use does not exist

--- tests/_marked.js
+++ tests/_marked.js
@@ -1,3 +1,11 @@
 import * as m from 'marked';
 
+if ('use' in m) {
+  // Test wrapper to handle v5 with breaking changes
+  m.use({
+    mangle: false,
+    headerIds: false
+  });
+}
+
 export default 'marked' in m ? m.marked : m.default;
