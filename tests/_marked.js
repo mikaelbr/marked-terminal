beb5969369e63e22c1c9170ba5f29b0c0bdaaaf8
Mikael Brevik <mikaelbre@gmail.com>
2023-05-11 08:42:07 +0200

    fix: cases where marked.use does not exist

--- tests/_marked.js
+++ tests/_marked.js
@@ -1,8 +1,11 @@
 import * as m from 'marked';
 
-m.use({
-  mangle: false,
-  headerIds: false
-});
+if ('use' in m) {
+  // Test wrapper to handle v5 with breaking changes
+  m.use({
+    mangle: false,
+    headerIds: false
+  });
+}
 
 export default 'marked' in m ? m.marked : m.default;
